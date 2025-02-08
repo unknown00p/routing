81c457e647e6e9fa77670c9760aa413825fdeb99
unknown00p <rajpoint8976@gmail.com>
2025-02-08 15:05:45 +0530

    commit

--- src/app/sequential-data-fetching/userData.tsx
+++ src/app/sequential-data-fetching/userData.tsx
@@ -1,24 +1,17 @@
 type DataType = {
     id: number;
     name: string;
-    email: string;
-    username: string;
-    phone: string;
   }
   
 async function UserData({userId}: {userId: number}) {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data: DataType[] = await response.json();
-    const user = data.filter((val)=> val.id == userId)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const data: DataType = await response.json();
+    // const user = data.filter((val)=> val.id == userId)
     return (
       <>
         <div>
           <div>
-            {user.map((val) => (
-              <div key={val.id}>
-                <p>{val.name}</p>
-              </div>
-            ))}
+            {data.name}
           </div>
         </div>
       </>
